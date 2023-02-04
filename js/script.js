4b4e6a28cf7090c2c395d3f2fb9cbf5d7fb86f32
Sylvia Gong <svagong@gmail.com>
2023-02-04 13:37:57 -0800

    fix hamburg

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 console.log("This probably won't stay here so I'm going to leave a smiley :)")
 var me = {name:"Sylvia", age: 26, hobbies:"dance"};
 console.log(me);
+
+//Nav menu toggle
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
