6e69c050d588e1c51af3ecdc7956bc57e9a29dac
Sylvia Gong <svagong@gmail.com>
2023-02-23 23:52:31 -0800

    adjustments_post_casestudy

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log("This probably won't stay here so I'm going to leave a smiley :)")
+console.log("This is a secret so I'm going to leave a smiley :)")
 var me = {name:"Sylvia", age: 26, hobbies:"dance"};
 console.log(me);
 
